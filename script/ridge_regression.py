6cd508583afbb6d6716012a432859dbc863097a7
DHYANI\DHYANI <vdhyani96@gmail.com>
2019-09-23 12:46:09 -0400

    Added mutliple iterations for different values of lambda parameter, plots and Leave One Out Cross Validation

--- script/ridge_regression.py
+++ script/ridge_regression.py
@@ -8,10 +8,9 @@ This is a temporary script file.
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics import mean_squared_error
-from sklearn.feature_selection import VarianceThreshold
 from math import sqrt
 import os
+from sklearn.feature_selection import VarianceThreshold
 import matplotlib.pyplot as plt
 
 
@@ -45,10 +44,10 @@ def perform_ridgeReg(X, y, lamda):
     X = X[:-1, :]
     term1 = np.matmul(wtrp, X)
     term2 = term1 + bias
-    term3 = term2 - y
+    term3 = term2.T - y
     obj2 = np.sum(term3 * term3)
     obj = obj1 + obj2
-    list = [w, bias, obj, Cinv]
+    list = [w, bias, obj, Cinv, obj1]
     return list
     
     
@@ -62,7 +61,7 @@ def ridgeReg(X, y, lamda):
     Cinv = params[3]
     ones = [[1 for i in range(X.shape[1])]]
     X = np.append(X, ones, axis = 0)
-    for i in range(200):
+    for i in range(X.shape[1]):
         xi = X[:, i]
         yi = y[i]
         expn1 = np.matmul(w.T, xi) - yi
@@ -72,7 +71,7 @@ def ridgeReg(X, y, lamda):
         loocv_error.append(error[0])
         
     w = w[:-1]
-    list = [w, bias, params[2], loocv_error]
+    list = [w, bias, params[2], loocv_error, params[4]]
     return list
 
 
@@ -83,7 +82,7 @@ def calc_rmse(pred, actual):
     mean_sqr_error = np.sum(sqr_error)/pred.shape[0]
     return sqrt(mean_sqr_error)
 
-#params = ridgeReg(np_train_dat, np_train_label, 0.01)
+#params = perform_ridgeReg(np_train_dat, np_train_label, 0.01)
 # Reading the data
 os.chdir('C:\\Users\\admin\\Desktop\\PostG\\GRE\\Second Take\\Applications\\Univs\\Stony Brook\\Fall 19 Courses\\ML\\Homeworks\\Homework 2\\Kaggle\\Data')
 train_data = pd.read_csv('trainData.csv', header = None)
@@ -107,13 +106,20 @@ val_label = pd.read_csv('valLabels.csv', header = None)
 val_label = val_label.iloc[:, 1:]
 np_val_label = val_label.values
 
+# Reading the Test Dataset
+test_data = pd.read_csv('testData_new.csv', header = None)
+test_data = test_data.iloc[:, 1:]
+np_test_dat = test_data.values
 
 # Modeling the training data now for different lamda
 rmse_train = []
 rmse_val = []
 rmse_loocv = []
-for lamda in [0.01, 0.1, 1, 10, 100, 1000]:
-    params = ridgeReg(np_train_dat, np_train_label, lamda)
+param_list = []
+lamda = [0.01, 0.1, 1, 10, 100, 1000]
+for l in lamda:
+    params = ridgeReg(np_train_dat, np_train_label, l)
+    param_list.append(params)
     wt = params[0]
     pred_train = np.matmul(np.transpose(np_train_dat), wt)
     pred_train = pred_train + params[1]     # adding bias term
@@ -130,47 +136,58 @@ for lamda in [0.01, 0.1, 1, 10, 100, 1000]:
     rmse = sqrt(loocv_mean)
     rmse_loocv.append(rmse)
     
+# Use 1 as lamda - minimum RMSE on validation data
+
+# Plotting the 3 RMSE against lamda values
+plt.plot(lamda, rmse_train,'r', lamda, rmse_val, 'g', lamda, rmse_loocv, 'b')
+plt.legend(['Train RMSE', 'Validation RMSE', 'Loocv RMSE'], loc='lower right')
+plt.show()
+# Taking log of lamda for better interpretation
+plt.plot(np.log(lamda), rmse_train,'r', np.log(lamda), rmse_val, 'g', np.log(lamda), rmse_loocv, 'b')
+plt.legend(['Train RMSE', 'Validation RMSE', 'Loocv RMSE'], loc='upper left')
+plt.show()
+
+
+## Best LOOCV performance
+index = rmse_loocv.index(min(rmse_loocv))
+opt_param = param_list[index]
+obj_val = opt_param[2]          # 24297.2194691486
+regl_term = opt_param[4]        # 11846.230194105075
+# sum of square errors on training data
+wt = opt_param[0]
+pred_train = np.matmul(np.transpose(np_train_dat), wt)
+pred_train = pred_train + params[1]
+error = pred_train - np_train_label
+sqr_error = error * error
+sum_sqr_error = np.sum(sqr_error)       # 79438.02458642688
+
+
+# Now identifying the top 10 most important and top 10 least important features
+id_no = pd.read_csv('featureTypes.txt', header = None)
+id_no = np.array(id_no.astype(str).values.tolist())
+wt = np.append(wt, id_no, axis = 1)
+sorted_weights = wt[wt[:, 0].argsort()]
+
+
 
 
 # Modeling the training + validation data
 train = np.append(np_train_dat, np_val_dat, axis = 1)
-#train = np.append(train, np_test_dat.T, axis = 1)
+train = np.append(train, np_test_dat.T, axis = 1)
 target = np.append(np_train_label, np_val_label, axis = 0)
 
 
 # removing low variance
-traint = train.T
+traint = train
 thresholder = VarianceThreshold(threshold = 0.00005)
 ab = thresholder.fit_transform(traint)
 train = ab[:10000, :].T
 np_test_dat = ab[10000:, :]
-params = ridgeReg(train, target, 0.01)
 
-
-
-
-# plotting correlation plot
-#dataframe = np.append(train.T, target, axis = 1)
-#dataframe = pd.DataFrame(data = dataframe)
-#subset = dataframe.iloc[:, 1:20]
-#subset.insert(19, '20', value = dataframe.iloc[:, -1])
-#corr = subset.corr()
-#corr.style.background_gradient(cmap = 'coolwarm')
-#plt.matshow(subset.corr())
-#plt.show()
-
-
-
-
-
-
-
+params = ridgeReg(train, target, 1)
 
 
 # Predicting on the test data
-test_data = pd.read_csv('testData_new.csv', header = None)
-test_data = test_data.iloc[:, 1:]
-np_test_dat = test_data.values
 wt = params[0]
 bias = params[1]
 prediction = np.matmul(np_test_dat, wt)
